e8cf4f27d827fe87502742a12a28bc90db281f7b
dmitriy <fokinds1@gmail.com>
2020-09-11 17:51:14 +0300

    first commit

---
+++ src/main/java/ru/fds/gatewayapi/GatewayApiApplication.java
@@ -0,0 +1,17 @@
+package ru.fds.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fds/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fds.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
